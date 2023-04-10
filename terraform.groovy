db4a069b9c6bbaa9e0e5e2d391cad0eae12bdff6
sriharitirumala <sritirumala30@gmail.com>
2023-04-10 09:16:20 +0530

    adding vpc subnets, routes, route table associations

--- terraform.groovy
+++ terraform.groovy
@@ -1,18 +1,32 @@
 def call() {
     pipeline {
         agent any
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
         stages {
-            stage('init') {
+
+            stage('Init') {
                 steps {
-                    sh 'terraform int -backend-config=env-dev/state.tfvars'
+                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
                 }
             }
 
-            stage('apply') {
+            stage('Apply') {
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    //sh 'terraform ${ACTION} -auto-approve -var-file=env-dev/main.tfvars'
+                    sh 'echo'
                 }
             }
+
         }
+
     }
 }
