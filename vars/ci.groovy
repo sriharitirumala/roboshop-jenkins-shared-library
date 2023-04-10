e8ddaaccd67b2e30c7f5844d1c25686803768c43
sriharitirumala <sritirumala30@gmail.com>
2023-04-10 14:19:17 +0530

    adding vpc subnets, routes, route table associations

---
+++ vars/ci.groovy
@@ -0,0 +1,20 @@
+def call (){
+    pipeline {
+        agent any
+
+        stages{
+            stage ("Compile/Build") {
+                steps{
+                    echo "Compile/Build"
+                }
+            }
+
+            stage ("Test Cases") {
+                steps{
+                    echo "Test Cases"
+                }
+            }
+
+        }
+    }
+}
