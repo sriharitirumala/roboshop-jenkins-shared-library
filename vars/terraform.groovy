6f6504a5e931c611b7a7991c14310db893d0494a
sriharitirumala <sritirumala30@gmail.com>
2023-04-15 11:18:20 +0530

    Ammend by SrihariHUB@gmail.com

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -15,8 +15,6 @@ def call() {
 
             stage('Init') {
                 steps {
-                    sh 'terraform init -migrate-state'
-                    sh 'terraform init -reconfigure'
                     sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
                 }
             }
