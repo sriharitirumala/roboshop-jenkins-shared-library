2937c8f6c96461a151fef697b837d13828eafe56
sriharitirumala <sritirumala30@gmail.com>
2023-04-20 09:58:06 +0530

    Ammend by SrihariHUB@gmail.com

--- vars/common.groovy
+++ vars/common.groovy
@@ -26,7 +26,7 @@ def codequality() {
 
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VERSION'
-    if (app_lang == "maven") {
+    if (app_lang == "nodejs") {
         sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
     } else {
         sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
