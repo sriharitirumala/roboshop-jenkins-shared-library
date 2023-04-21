1c6984d6944737bad32627ff92e2ef571f9aaf8d
sriharitirumala <sritirumala30@gmail.com>
2023-04-21 11:20:38 +0530

    Ammend by SrihariHUB@gmail.com

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -6,29 +6,25 @@ def call() {
             string(name: 'app_version', defaultValue: '', description: 'App Version')
             string(name: 'component', defaultValue: '', description: 'Component')
             string(name: 'environment', defaultValue: '', description: 'Environment')
+        }
 
-            stages {
-                stage('update parameter store') {
-                    steps {
-                        sh 'aws ssm put-parameter --name ${environment}.${component}.app_version  --type "String" --value "${app_version}" --overwrite'
-                    }
-
+        stages {
+            stage('update parameter store') {
+                steps {
+                    sh 'aws ssm put-parameter --name ${environment}.${component}.app_version  --type "String" --value "${app_version}" --overwrite'
                 }
+            }
 
-                stage('deploy servers') {
-                    steps {
-                        script {
-                            env.SSH_PASSWORD = sh(script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true).trim()
-                            wrap([$class          : 'MaskPasswordsBuildWrapper',
-                                  varPasswordPairs: [[password: SSH_PASSWORD]]]) {
-                                sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
-                                sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
-                            }
+            stage('deploy servers') {
+                steps {
+                    script {
+                        env.SSH_PASSWORD = sh(script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true).trim()
+                        wrap([$class          : 'MaskPasswordsBuildWrapper',
+                              varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+                            sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+                            sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
                         }
-
-
                     }
-
                 }
             }
         }
