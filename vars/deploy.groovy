369c8eeb34665e1985deb8fd68fa0506da1cc957
sriharitirumala <sritirumala30@gmail.com>
2023-04-21 11:09:07 +0530

    Ammend by SrihariHUB@gmail.com

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -17,8 +17,15 @@ def call() {
 
                 stage('deploy servers') {
                     steps {
-                        sh 'aws ec2 describe-instances --filters "Name=tag:Name, value=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers '
-                        sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
+                        script {
+                            env.SSH_PASSWORD = sh(script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true).trim()
+                            wrap([$class          : 'MaskPasswordsBuildWrapper',
+                                  varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+                                sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
+                                sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
+                            }
+                        }
+
 
                     }
 
