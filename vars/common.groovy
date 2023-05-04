c8dc618beb149aeed8e1e7b076095edf4711de8f
sriharitirumala <sritirumala30@gmail.com>
2023-05-04 15:11:56 +0530

    updates

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,13 +25,13 @@ def codequality() {
 }
 
 def prepareArtifacts() {
-    sh 'echo ${TAG_NAME} >VERSION'
-    if (app_lang == "maven") {
-        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
-    } else {
-        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-    }
-
+    // sh 'echo ${TAG_NAME} >VERSION'
+    // if (app_lang == "maven") {
+    //     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+    // } else {
+    //     sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+    // }
+    docker build -t 119044227534.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .
 }
 
 def artifactUpload() {
